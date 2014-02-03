a89077945f403286a67ccde33e561bca28ad2c68
m0072 <m0072@W7AVPM0072.aviva.mad>
2014-02-03 12:19:47 +0100

    Primer commit

---
+++ src/calculofirmatlp/CalculoFirmaTLP.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculofirmatlp;
+
+/**
+ *
+ * @author Santiago
+ */
+public class CalculoFirmaTLP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Principal ventanaPrincipal = new Principal();
+        ventanaPrincipal.setVisible(true);
+        
+    }
+}
